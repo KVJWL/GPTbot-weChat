8927891f67b35b35374c663c3d27f847ea8ff4c9
KVJWL <126389781+KVJWL@users.noreply.github.com>
2023-02-27 02:17:27 +0800

    Add files via upload

---
+++ src/main.ts
@@ -0,0 +1,131 @@
+import {Contact, Message, ScanStatus, WechatyBuilder} from "wechaty";
+import {PuppetPadlocal} from "wechaty-puppet-padlocal";
+import QRCode from "qrcode";
+import { ChatGPTBot } from "./bot.js";
+const chatGPTBot = new ChatGPTBot();
+
+// const bot =  WechatyBuilder.build({
+//   name: "wechat-assistant", // generate xxxx.memory-card.json and save login data for the next login
+  // puppetOptions: {
+  //   uos: true, // 开启uos协议
+  // },
+  // puppet: "wechaty-puppet-wechat",
+// });
+
+// const token: string = "puppet_padlocal_72ee99189d5648b9baf70c2d24f7d808"; // padlocal token
+
+const token: string = "puppet_padlocal_033ca17230264abdb7a38d04f37c1243"; // padlocal token
+const puppet = new PuppetPadlocal({ token }); 
+const bot =  WechatyBuilder.build({
+  name: "chatgptBot",
+  puppet,
+});
+
+// get a Wechaty instance
+
+async function main() {
+  await chatGPTBot.startGPTBot();
+
+  bot
+  // .on("scan", async (qrcode, status) => {
+  //   const url = `https://wechaty.js.org/qrcode/${encodeURIComponent(qrcode)}`;
+  //   console.log(`Scan QR Code to login: ${status}\n${url}`);
+  //   console.log(
+  //     await QRCode.toString(qrcode, { type: "terminal", small: true })
+  //   );
+  // })
+  .on("scan", (qrcode: string, status: ScanStatus) => {
+    if (status === ScanStatus.Waiting && qrcode) {
+        const qrcodeImageUrl = ["https://api.qrserver.com/v1/create-qr-code/?data=", encodeURIComponent(qrcode)].join("");
+        console.log(`onScan: ${ScanStatus[status]}(${status}) - ${qrcodeImageUrl}`);
+        console.log(
+             QRCode.toString(qrcode, { type: "terminal", small: true })
+          );
+    } else {
+        console.log(`onScan: ${ScanStatus[status]}(${status})`);
+    }
+  })
+
+  // .on("login", async (user) => {
+  //   console.log(`User ${user} logged in`);
+  //   chatGPTBot.setBotName(user.name());
+  // })
+
+  .on("login", (user: Contact) => {
+    console.log(`User ${user} logged in`);
+    chatGPTBot.setBotName(user.name());
+  })
+  .on("message", async (message: Message) => {
+    if (!chatGPTBot.ready) {
+      return;
+    }
+    if (message.text().startsWith("/ping")) {
+      await message.say("pong");
+      return;
+    }
+    try {
+      console.log(`Message: ${message}`);
+      await chatGPTBot.onMessage(message);
+    } catch (e) {
+      console.error(e);
+    }
+  })
+  // .on("message", async (message) => {
+  //   if (!chatGPTBot.ready) {
+  //     return;
+  //   }
+  //   if (message.text().startsWith("/ping")) {
+  //     await message.say("pong");
+  //     return;
+  //   }
+  //   try {
+  //     console.log(`Message: ${message}`);
+  //     await chatGPTBot.onMessage(message);
+  //   } catch (e) {
+  //     console.error(e);
+  //   }
+  // });
+try {
+  await bot.start();
+} catch (e) {
+  console.error(
+    `⚠️ Bot start failed, can you log in through wechat on the web?: ${e}`
+  );
+}
+//======================== puppet: "wechaty-puppet-wechat"=======================
+//   bot
+//     .on("scan", async (qrcode, status) => {
+//       const url = `https://wechaty.js.org/qrcode/${encodeURIComponent(qrcode)}`;
+//       console.log(`Scan QR Code to login: ${status}\n${url}`);
+//       console.log(
+//         await QRCode.toString(qrcode, { type: "terminal", small: true })
+//       );
+//     })
+//     .on("login", async (user) => {
+//       console.log(`User ${user} logged in`);
+//       chatGPTBot.setBotName(user.name());
+//     })
+//     .on("message", async (message) => {
+//       if (!chatGPTBot.ready) {
+//         return;
+//       }
+//       if (message.text().startsWith("/ping")) {
+//         await message.say("pong");
+//         return;
+//       }
+//       try {
+//         console.log(`Message: ${message}`);
+//         await chatGPTBot.onMessage(message);
+//       } catch (e) {
+//         console.error(e);
+//       }
+//     });
+//   try {
+//     await bot.start();
+//   } catch (e) {
+//     console.error(
+//       `⚠️ Bot start failed, can you log in through wechat on the web?: ${e}`
+//     );
+//   }
+}
+main();
